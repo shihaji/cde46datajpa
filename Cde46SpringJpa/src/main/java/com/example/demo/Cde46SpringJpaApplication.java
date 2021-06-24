715ed6d32de70a5a49e3101ec460a2a8fc7859fe
shihaji <shihajig@gmail.com>
2021-06-24 14:57:03 +0530

    commit

--- Cde46SpringJpa/src/main/java/com/example/demo/Cde46SpringJpaApplication.java
+++ Cde46SpringJpa/src/main/java/com/example/demo/Cde46SpringJpaApplication.java
@@ -1,8 +1,12 @@
 package com.example.demo;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.data.domain.Sort;
 
 @SpringBootApplication
 public class Cde46SpringJpaApplication {
@@ -13,15 +17,27 @@ public class Cde46SpringJpaApplication {
 	
 		EmployeeRepository empRepo=ctx.getBean(EmployeeRepository.class);
 		
-		Employee emp=empRepo.findById(1).get();
+		//List<Employee> list=empRepo.findAll(Sort.by(Sort.Direction.DESC,"name"));
 		
-		emp.setName("cts");
-		
-		empRepo.save(emp);
-		
-		
+		//for(Employee e:list) {
+		//	System.out.println(e.getName());
+		//}
 		
 		
+		/*
+		 * List<Employee> list=empRepo.getAllEmp();
+		 * 
+		 * for(Employee e:list) { System.out.println(e.getName()+"   "+e.getSalary()); }
+		 */
+		
+		//Employee emp=empRepo.findByName("cts");
+		
+		
+		List<Employee> list=empRepo.findByNameIn(Arrays.asList("cts","abc","xyz"));
+		
+		for(Employee e:list) {
+			System.out.println(e.getName()+"   "+e.getSalary());
+		}
 	
 	
 	}
--- Cde46SpringJpa/src/main/java/com/example/demo/EmployeeRepository.java
+++ Cde46SpringJpa/src/main/java/com/example/demo/EmployeeRepository.java
@@ -1,9 +1,51 @@
 package com.example.demo;
 
+import java.util.Collection;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer>{
+	
+	@Query("select e from Employee e where e.salary>?1")
+	public List<Employee> getEmpAsSalary(int salary);
+	
+	@Query("select e from Employee e where e.id>:id")
+	public List<Employee> getEmpAsid(int id);
+	
+	@Query(value = "select * from employee",nativeQuery = true)
+	public List<Employee> getAllEmp();
+		
+	
+	public Employee findByName(String name);
+	
+	
+	public Employee findByNameAndId(String name,int id);
+	
+	
+	public List<Employee> getByNameOrSalary(String name,int salary);
+	
+	public List<Employee> findBySalaryBetween(int start,int end);
+	
+	public List<Employee> findBySalaryLessThan(int salary);
+	
+	public List<Employee> findBySalaryGreaterThan(int salary);
+	
+	public List<Employee> findByNameIsNull();
+	
+	public List<Employee> findByNameNotNull();
+	
+	public List<Employee> findByNameLike(String pattern);
+	
 
+	public List<Employee> findByNameNotLike(String pattern);
+	
+	public List<Employee> findByNameNot(String name);
+	
+	public List<Employee> findByNameNotIn(Collection<String> list);
+	
+	public List<Employee> findByNameIn(Collection<String> list);
 }
